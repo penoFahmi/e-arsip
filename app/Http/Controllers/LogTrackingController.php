e828f743f0393b05bd038a6aa22836375dfc7fe8
penoFahmi <penofahmi@gmail.com>
2025-11-23 20:08:07 +0700

    first-commit

---
+++ app/Http/Controllers/LogTrackingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LogTracking;
+use Illuminate\Http\Request;
+
+class LogTrackingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LogTracking $logTracking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LogTracking $logTracking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LogTracking $logTracking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LogTracking $logTracking)
+    {
+        //
+    }
+}
